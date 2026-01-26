dd7d2d21f5d3c1b47d47dc6b0de5fae054a7934e
Mikołaj Rosowski <m.rosowski1@wp.pl>
2026-01-26 13:41:10 +0100

    Initial commit

---
+++ include/torch_consts_generated.h
@@ -0,0 +1,50 @@
+/* AUTOMATICALLY GENERATED AT 2026-01-25 22:19:53.281317 */
+#ifndef TORCH_CONSTS_GENERATED
+#define TORCH_CONSTS_GENERATED
+/* CONSTS */
+#define SR 16000
+#define N_MELS 40
+#define N_FFT 512
+#define N_FRAMES 63
+#define N_BANKS 40
+/* Labels */
+static const char* LABELS[] = {
+"backward",
+"bed",
+"bird",
+"cat",
+"dog",
+"down",
+"eight",
+"five",
+"follow",
+"forward",
+"four",
+"go",
+"happy",
+"house",
+"learn",
+"left",
+"marvin",
+"nine",
+"no",
+"off",
+"on",
+"one",
+"right",
+"seven",
+"sheila",
+"six",
+"stop",
+"three",
+"tree",
+"two",
+"up",
+"visual",
+"wow",
+"yes",
+"zero",
+"unknown",
+"silence",
+};
+#endif /* TORCH_CONSTS_GENERATED */
